7f3bf03d0fcadba41e6500b6ffcd65b3b728a288
Aldinonid <aldino.efendi7@gmail.com>
2020-10-21 03:53:24 +0700

    Move Login & Register page to Member Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import RegsiterPage from "pages/RegisterPage";
-import LoginPage from "pages/LoginPage";
 import LandingPage from "pages/LandingPage";
 import ClassPage from "pages/ClassPage";
 import FlowPage from "pages/FlowPage";
@@ -18,8 +16,6 @@ function App() {
         <Route exact path="/class" component={ClassPage} />
         <Route exact path="/class/:slug" component={DetailPage} />
         <Route exact path="/flow" component={FlowPage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/register" component={RegsiterPage} />
       </Router>
     </div>
   );
